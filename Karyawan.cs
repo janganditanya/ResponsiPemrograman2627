c76d1231bf9e00607d9cf4228ac71e6e94b060ff
janganditanya <joseph.w@students.amikom.ac.id>
2020-04-13 15:05:57 +0700

    finishing project

---
+++ Karyawan.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2627 
+{
+    public class Karyawan 
+    {
+        public string NIK { get; set; }
+        public string Nama { get; set; }
+        public double GajiBulanan { get; set; }
+        public string No { get; set; }
+
+        public void GetData() 
+        {
+            Console.WriteLine("{0} {1} {2}\t{3}", No, NIK, Nama, GajiBulanan);
+        }
+
+        public void GetDataNaik() 
+        {
+            double temp;
+            temp = GajiBulanan * 0.1;
+            GajiBulanan += Convert.ToInt32(temp);
+            Console.WriteLine("{0} {1} {2}\t{3}", No, NIK, Nama, GajiBulanan);
+        }
+
+        public Karyawan(string no, string nik, string nama, double gajibulanan) 
+        {
+            No = no;
+            NIK = nik;
+            Nama = nama;
+            GajiBulanan = gajibulanan;
+
+            if (GajiBulanan < 0) 
+            {
+                GajiBulanan = 0;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2627 
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan1 = new Karyawan("1.", "190302124", "Riki", 3000000);
+            Karyawan karyawan2 = new Karyawan("2.", "190302123", "Maru", 2000000);
+
+            Console.WriteLine("No. NIK/Nama\t\tGaji Bulanan");
+            Console.WriteLine("-------------------------------");
+
+            karyawan1.GetData();
+            karyawan2.GetData();
+
+            Console.WriteLine("\nAseeeek Naik Gaji 10%!!!\n");
+            Console.WriteLine("No. NIK/Nama\t\tGaji Bulanan");
+            Console.WriteLine("-------------------------------");
+
+            karyawan1.GetDataNaik();
+            karyawan2.GetDataNaik();
+
+            Console.ReadKey();
+        }
+    }
+}
